68b78b6018687e96f34fa8cd9878e35d9bb7c490
JONATHANDEMOND <133927721+JONATHANDEMOND@users.noreply.github.com>
2024-05-10 10:55:58 -0500

    comit

--- src/app/services/empleados.service.ts
+++ src/app/services/empleados.service.ts
@@ -8,7 +8,7 @@ import { Observable } from 'rxjs';
 export class EmpleadosService {
 
   constructor(private http:HttpClient) { }
-private API_EMPELADOS='https://jritsqmet.github.io/web-api/crash.json'
+private API_EMPELADOS='https://jritsqmet.github.io/web-api/crash'
 
 getEmpelado():Observable<any>{
   return this.http.get(this.API_EMPELADOS)
